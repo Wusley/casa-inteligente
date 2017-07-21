3b38d4821ed36f07f1ec2b11c16e02cc7dffa744
Wesley Lima <contato@wesley.pro.br>
2017-07-21 18:49:46 -0300

    Criado animacao uau

--- CasaInteligente/src/App.js
+++ CasaInteligente/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import { vw, vh } from 'react-native-viewport-units';
 import {
 	StyleSheet,
+	ScrollView,
 	View,
 	Text,
 	Image
@@ -19,38 +20,70 @@ var vw = width/100,
 
 const App = () => {
   return (
-    <View>
-    	<View style={ styles.lineTools }></View>
-    	<View style={ styles.profile }>
-    		<View style={ styles.thumb }>
-    			<Image source={{ uri: 'https://res.cloudinary.com/dwqwnm1r6/image/upload/v1499808665/house_uwlxiy.png' }} style={ styles.imageThumb } />
-    		</View>
-        	<View style={ styles.boxPart }></View>
+    <View style={ styles.main }>
+    	<View style={ styles.mainFix }>
+	    	<View style={ styles.profile }></View>
+	    	<View style={ styles.boxPart }></View>
+	    	<Animatable.View style={ styles.lineTools } animation="nav" delay={500} iterationCount="1"></Animatable.View>
+	    	<Animatable.View style={ styles.thumb } animation="bounceIn" iterationCount="1">
+					<Image source={{ uri: 'https://res.cloudinary.com/dwqwnm1r6/image/upload/v1499808665/house_uwlxiy.png' }} style={ styles.imageThumb } />
+				</Animatable.View>
+
+				<ScrollView style={ styles.tools }>
+	        <View style={ styles.tool }>
+              <Text style={ styles.name }>Regador</Text>
+              <Text style={ styles.time }>
+								<Text style={ styles.started }>9:02</Text>
+							</Text>
+	        </View>
+	      </ScrollView>
     	</View>
     </View>
   );
 };
 
+Animatable.initializeRegistryWithDefinitions({
+	nav: {
+	  from: {
+	    height: '0%',
+	  },
+	  to: {
+	    height: '100%',
+	  }
+	}
+} );
+
+// Animatable.initializeRegistryWithDefinitions( fadeIn );
+
 const styles = StyleSheet.create( {
+	main: {
+		backgroundColor: 'white',
+		width: '100%',
+		height: '100%'
+	},
+	mainFix: {
+		height: '100%'
+	},
 	profile: {
 		position: 'relative',
 		display: 'flex',
 		width: '100%',
-		height: 45*vw,
+		height: 40*vw,
 		backgroundColor: '#6c5b7b',
-		zIndex: 9
+		zIndex: 0
 	},
 	thumb: {
 		position: 'absolute',
 		flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-		bottom: 15*vw,
-		left: 5*vw,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+		top: 10*vw,
+		left: 10*vw,
 		width: 20*vw,
 		height: 20*vw,
 		backgroundColor: '#f8aa80',
+		// opacity: 0,
 		// backgroundImage: {
 
 		// },
@@ -59,12 +92,13 @@ const styles = StyleSheet.create( {
 		// background-size: 80%;
 		// background-position: center;
 		borderRadius: 20*vw,
+		elevation: 5,
 
 		// transition: all .5s linear;
 
 	    // transform: scale(0, 0);
-	    // box-shadow: 0 2px 5px 1px rgba( 0, 0, 0, .2 );
-		zIndex: 9
+
+		zIndex: 3
 	},
 	imageThumb: {
 		width: '80%',
@@ -75,7 +109,7 @@ const styles = StyleSheet.create( {
 		width: '110%',
 		height: 40*vw,
 		left: -5*vw,
-		bottom: -20*vw,
+		top: 25*vw,
 		backgroundColor: 'white',
 		transform: [{rotate: '7deg'}],
 		zIndex: 1
@@ -85,12 +119,10 @@ const styles = StyleSheet.create( {
 		width: 1.2*vw,
 		backgroundColor: 'rgba( 0, 0, 0, 0.05 )',
 		top: 25*vw,
-		left: 14.5*vw,
+		left: 19.5*vw,
 		height: '100%',
-		zIndex: 1
+		zIndex: 2
 	}
 } );
 
 export default App;
-
-
